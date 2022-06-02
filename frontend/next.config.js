e2f23d2823571056b854aedb1bbe0e75e5916bef
Akshat mangal <mangalakshat01@gmail.com>
2022-06-02 16:15:23 +0530

    Added a function to fetch list of all exchanges

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  env: {
+    BASE_API: process.env.BASE_API,
+    API_KEY: process.env.API_KEY,
+  },
 }
 
 module.exports = nextConfig
--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,8 +1,28 @@
 import type { NextPage } from 'next'
+import { useEffect } from 'react'
 import Head from 'next/head'
 import Image from 'next/image'
+import axios from 'axios'
 
 const Home: NextPage = () => {
+  
+  useEffect(() => {
+    
+    // fetches complete list of available exchanges and returns it in the form of array
+    const fetchData = async () => {
+      const res = await axios.get(process.env.BASE_API+'/exchanges/', {
+        headers: {
+         'X-CoinAPI-Key': process.env.API_KEY!
+        }
+      });
+      console.log(res);
+    }
+    fetchData();
+
+  })
+
+
+
   return (
     <h1 className="text-3xl font-bold underline text-red-400">
       Crypto Exchange!
