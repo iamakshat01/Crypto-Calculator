28828138fdc18f51e2be079bd557e05fb9a1374b
Akshat mangal <mangalakshat01@gmail.com>
2022-06-06 16:54:37 +0530

    Added reverse button

--- frontend/components/ui/SelectDropdown.tsx
+++ frontend/components/ui/SelectDropdown.tsx
@@ -5,7 +5,8 @@ import { dataType,exchangeInput } from '../../data/types'
 interface DropdownProps{
   data: any,
   fillerText: string,
-  onChange: React.Dispatch<React.SetStateAction<exchangeInput>>
+  parent?: exchangeInput,
+  onChange: any
 }
 
 
@@ -14,26 +15,15 @@ const SelectDropdown = (props:DropdownProps)  => {
     return (
       <div className="w-full max-w-xs flex-initial">
         <Select
+          value={props.parent}
           className="basic-single"
           classNamePrefix="select"
-          defaultValue={props.fillerText}
           isClearable={true}
           isSearchable={true}
           name="color"
           options={props.data}
           onChange={props.onChange}
         />
-
-        <div
-          style={{
-            color: 'hsl(0, 0%, 40%)',
-            display: 'inline-block',
-            fontSize: 12,
-            fontStyle: 'italic',
-            marginTop: '1em',
-          }}
-        >
-        </div>
       </div>
     );
 }
